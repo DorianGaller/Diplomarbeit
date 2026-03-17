2cba57efaf5c93d257d0f29cf898f0e46fcaba3a
MopsAndi <andrespiegel09@gmail.com>
2026-03-17 15:51:13 +0100

    bug fixes

--- Assets/Assets_Mops/Level/Prefab Interactable Tiles/Vent/VentManager.cs
+++ Assets/Assets_Mops/Level/Prefab Interactable Tiles/Vent/VentManager.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 
 public class VentManager : MonoBehaviour
 {
-    [Header("ON ENTER VENT")]
+    [Header("PLAYER")]
+    public MonoBehaviour playerMovement;
+
+    // -------------------- GAMEOBJECTS --------------------
+
+    [Header("ON ENTER VENT - OBJECTS")]
     public List<GameObject> enableOnEnter = new List<GameObject>();
     public List<GameObject> disableOnEnter = new List<GameObject>();
 
-    [Header("ON EXIT VENT")]
+    [Header("ON EXIT VENT - OBJECTS")]
     public List<GameObject> enableOnExit = new List<GameObject>();
     public List<GameObject> disableOnExit = new List<GameObject>();
 
-    private bool isInVent = false;
+    // -------------------- 2D COLLIDERS (SPRITES!) --------------------
 
+    [Header("ON ENTER VENT - 2D COLLIDERS")]
+    public List<Collider2D> enable2DCollidersOnEnter = new List<Collider2D>();
+    public List<Collider2D> disable2DCollidersOnEnter = new List<Collider2D>();
+
+    [Header("ON EXIT VENT - 2D COLLIDERS")]
+    public List<Collider2D> enable2DCollidersOnExit = new List<Collider2D>();
+    public List<Collider2D> disable2DCollidersOnExit = new List<Collider2D>();
+
+    // -------------------- STATE --------------------
+
+    private bool isInVent = false;
     public bool IsInVent => isInVent;
+
+    // -------------------- MAIN --------------------
 
     public void ToggleVent()
     {
+        // Sicherheit: Movement immer aktiv
+        if (playerMovement != null)
+            playerMovement.enabled = true;
+
         if (!isInVent)
-        {
             EnterVent();
-        }
         else
-        {
             ExitVent();
-        }
 
         isInVent = !isInVent;
     }
@@ -32,14 +50,26 @@ public class VentManager : MonoBehaviour
     private void EnterVent()
     {
         ToggleObjects(enableOnEnter, disableOnEnter);
+        Toggle2DColliders(enable2DCollidersOnEnter, disable2DCollidersOnEnter);
+
+        if (playerMovement != null)
+            playerMovement.enabled = true;
+
         ClearUIFocus();
     }
 
     private void ExitVent()
     {
         ToggleObjects(enableOnExit, disableOnExit);
+        Toggle2DColliders(enable2DCollidersOnExit, disable2DCollidersOnExit);
+
+        if (playerMovement != null)
+            playerMovement.enabled = true;
+
         ClearUIFocus();
     }
+
+    // -------------------- HELPERS --------------------
 
     private void ToggleObjects(List<GameObject> enableList, List<GameObject> disableList)
     {
@@ -56,10 +86,25 @@ public class VentManager : MonoBehaviour
         }
     }
 
+
+    private void Toggle2DColliders(List<Collider2D> enableList, List<Collider2D> disableList)
+    {
+        foreach (Collider2D col in enableList)
+        {
+            if (col != null)
+                col.enabled = true;
+        }
+
+        foreach (Collider2D col in disableList)
+        {
+            if (col != null)
+                col.enabled = false;
+        }
+    }
+
     private void ClearUIFocus()
     {
         if (UnityEngine.EventSystems.EventSystem.current != null)
-            UnityEngine.EventSystems.EventSystem.current
-                .SetSelectedGameObject(null);
+            UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
     }
 }
